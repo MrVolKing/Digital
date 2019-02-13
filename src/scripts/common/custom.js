8a0a0413dc698ca5e3f0652fc1321447755f819f
mrKing <MrKing>
2019-02-13 12:45:01 +0200

    multilang2

--- src/scripts/common/custom.js
+++ src/scripts/common/custom.js
@@ -71,16 +71,17 @@ $(document).ready(() => {
 
 
     // slider
-    var menu1 = ['Хранение информации о клиентах <b>в подходящем для AI формате</b> ', '<b>Выбор предложения</b>, креатива, заголовка для каждого получателя с помощью AI', '<b>Построение на основе AI сегментов</b>  для массовых коммуникаций под конкретные предложения', '<b>Выбор с помощью AI</b> наилучших предложений под созданные сегменты'];
-    var menu2 = ['<b>Неограниченный набор условий</b> для персонализации путешествия клиента', '<b>Автовыбор контента</b> для каждого получателя с помощью AI', '<b>Реакция на действия клиента</b> в других системах (CRM, сайт)', 'Автоматизация кросс-канальных коммуникаций <b>любой сложности</b>'];
-    var menu3 = ['Возможность использовать свои сервера для отправки email или <b>транзакционный API</b>', '<b>Продвинутый</b> планировщик массовых рассылок ', '<b>Inbox-мониторинг</b> и гибкое управление рассылками', '<b>Автоматическая проверка</b> базы по внутренним и внешним black-листам', '<b>Уникальная инфраструктура</b>, позволяющая AI подключиться к управлению маркетингом'];
-    var menu4 = ['<b>Детальный контроль</b> дохода с каждой кампании, предложения, креатива', '<b>Контроль качества</b> доставки по каналам рассылок', '<b>Кастомные параметры</b> для отслеживания внешних событий и условий'];
+    // var menu1 = ['Хранение информации о клиентах <b>в подходящем для AI формате</b> ', '<b>Выбор предложения</b>, креатива, заголовка для каждого получателя с помощью AI', '<b>Построение на основе AI сегментов</b>  для массовых коммуникаций под конкретные предложения', '<b>Выбор с помощью AI</b> наилучших предложений под созданные сегменты'];
+    // var menu2 = ['<b>Неограниченный набор условий</b> для персонализации путешествия клиента', '<b>Автовыбор контента</b> для каждого получателя с помощью AI', '<b>Реакция на действия клиента</b> в других системах (CRM, сайт)', 'Автоматизация кросс-канальных коммуникаций <b>любой сложности</b>'];
+    // var menu3 = ['Возможность использовать свои сервера для отправки email или <b>транзакционный API</b>', '<b>Продвинутый</b> планировщик массовых рассылок ', '<b>Inbox-мониторинг</b> и гибкое управление рассылками', '<b>Автоматическая проверка</b> базы по внутренним и внешним black-листам', '<b>Уникальная инфраструктура</b>, позволяющая AI подключиться к управлению маркетингом'];
+    // var menu4 = ['<b>Детальный контроль</b> дохода с каждой кампании, предложения, креатива', '<b>Контроль качества</b> доставки по каналам рассылок', '<b>Кастомные параметры</b> для отслеживания внешних событий и условий'];
     
 
-    // var menu1 = ['dn'];
-    // var menu2 = [];
-    // var menu3 = [];
-    // var menu4 = [];
+    var menu1 = ['Customers data storage suitable for <b>AI-driven Marketing</b> ', '<b>AI-suggested best offer</b>, creative, and subject-line for one-on-one communication', '<b>Intelligent auto segments</b> driven by specific offers for highly effective direct marketing', '<b>Next best offer recommendation</b> delivered by machine learning for your scheduled segments'];
+    var menu2 = ['<b>Unlimited set of conditions</b> for personalizing a customer\'s journey', '<b>Automated personalized content</b> for each customer through intelligent algorithms', '<b>Continuous performance improvement</b> by taking into account customer behavior data from across sources (CRM, website etc.)', 'Cross-channel campaigns of any point of complexity to deliver <b>exceptional one-on-one experiences at scale</b>'];
+    var menu3 = ['Ability to send emails through <b>your own SMTP-servers or preferred transactional API </b>', 'Advanced bulk-campaigns <b>scheduling with built-in suggestions driven by AI</b>', 'Inbox-monitoring and <b>agile mailings-management infrastructure</b>', 'Automatic database <b>check against internal and external blacklists</b>', 'First-in-class platform that enables <b>AI-managed marketing automation</b>'];
+    var menu4 = ['<b>Detailed breakdown report</b> by campaign, offer, or creative to improve your marketing ROI', '<b>Deliverability insights</b> per each sending channel', 'Custom parameters for <b>tracking external events and bringing together data from across sources</b>'];
+    
 
     var swiper = new Swiper('.swiper-container1', {
         loop: false,
